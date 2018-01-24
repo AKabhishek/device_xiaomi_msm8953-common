689fa754982a0da131117d2a0c33bb64fcfe903f
AKabhishek <abhishekkaushik.star@gmail.com>
2018-01-24 16:29:33 +0000

    Lineage >> AOS
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,9 +24,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+AOS_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$AOS_ROOT"/vendor/aos/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -34,7 +34,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AOS_ROOT" true
 
 # Copyright headers and guards
 write_headers "mido tissot"
